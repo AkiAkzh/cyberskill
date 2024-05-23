793141d58ec74b8771e9a87d5c890bc1bb6a7228
Akzhan Mataev <150265945+AkiAkzh@users.noreply.github.com>
2024-05-23 17:14:08 +0500

    create server and auth. change some code in client

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { createContext } from "react";
 import { createRoot } from "react-dom/client";
-import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App"
 import "./global.css";
+import Store from "./store/store";
+
+interface State{
+  store : Store,
+ }
+
+const store = new Store();
+
+export const Context = createContext<State>({
+  store,
+})
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
 
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Context.Provider value={{store}}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Context.Provider>
 );
