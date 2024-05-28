5469f3cbd3f7723e161434b1a46712f919bfa4e2
Margulan Zhumabek <dxskull9@gmail.com>
2024-05-28 13:29:07 +0500

    CoursePagePentest added and modified, dockers completed

--- client/src/pages/CoursePageCryptography.tsx
+++ client/src/pages/CoursePageCryptography.tsx
@@ -12,7 +12,7 @@ const HintPopup: FunctionComponent<{ hint: string, onClose: () => void }> = ({ h
   </div>
 );
 
-const CoursePage: FunctionComponent = () => {
+const CoursePageCryptography: FunctionComponent = () => {
   const [isOpenArray, setIsOpenArray] = useState(new Array(3).fill(false));
 
   const toggleSection = (index: number) => {
@@ -68,142 +68,6 @@ const CoursePage: FunctionComponent = () => {
       <main className="coursesectionscontainer">
         <div className="coursesectioncontainer" onClick={() => toggleSection(0)} >
           <div className="coursesectionbox">
-            <div className="checkboxgraycontainer">
-              <img className="checkboxgray-icon" alt="" src="/checkboxgray.svg" />
-            </div>
-            <div className="coursesectionnametext">Passive Reconnaissance</div>
-          </div>
-          <div className="arrowdowncontainer">
-            <img className="arrowdownicon" alt="" src="/arrowdownicon.svg" />
-          </div>
-        </div>
-        {isOpenArray[0] && (
-          <div className="course-section-dropdown-container">
-            <p className="course-section-paragraph">
-              In essence, Burp Suite is a Java-based framework designed to
-              serve as a comprehensive solution for conducting web application
-              penetration testing. It has become the industry standard tool
-              for hands-on security assessments of web and
-              mobile applications, including those that rely
-              on application programming interfaces (APIs).
-            </p>
-            <p className="course-section-paragraph">
-              Simply put, Burp Suite captures and enables manipulation of all
-              the HTTP/HTTPS traffic between a browser and a web server. This
-              fundamental capability forms the backbone of the framework. By
-              intercepting requests, users have the flexibility to route them
-              to various components within the Burp Suite framework, which we
-              will explore in upcoming sections. The ability to intercept,
-              view, and modify web requests before they reach the target
-              server or even manipulate responses before they are received by
-              our browser makes Burp Suite an invaluable tool for manual web
-              application testing.
-            </p>
-
-            <img
-              className="image1-icon"
-              loading="lazy"
-              alt=""
-              src="/image1@2x.png"
-            />
-            <p className="course-section-paragraph">
-              Before the dawn of computer systems and networks, in the Art of
-              War, Sun Tzu taught, “If you know the enemy and know yourself,
-              your victory will not stand in doubt.” If you are playing the
-              role of an attacker, you need to gather information about your
-              target systems. If you are playing the role of a defender, you
-              need to know what your adversary will discover about your
-              systems and networks.
-            </p>
-            
-            <p className="course-section-paragraph">
-              Reconnaissance <i> (recon) </i> can be defined as a preliminary survey to gather information
-              about a target. It is the first step in <b > The Unified Kill Chain </b> to gain an initial foothold on a system. We divide reconnaissance into:
-              <ol className="">
-                <li className="">
-                  Passive Reconnaissance
-                </li>
-                <li>
-                  Active Reconnaissance
-                </li>
-              </ol>
-            </p>
-
-            <img
-              className="image"
-              loading="lazy"
-              alt=""
-              src="/image2@2x.png"
-            />
-
-            <p className="course-section-paragraph">
-              This record shows a web server;
-              <i> however, as mentioned already, Shodan.io collects information related to any device it can find connected online. </i>
-              Searching for
-              <b>tryhackme.com</b> on Shodan.io will display at least the record shown in the
-              screenshot above. Via this Shodan.io search result, we can
-              learn several things related to our search, such as:
-              <ul className="">
-                <li className="">
-                  IP address
-                </li>
-                <li className="">
-                  hosting company
-                </li>
-                <li className="">
-                  geographic location
-                </li>
-                <li>
-                  server type and version
-                </li>
-              </ul>
-            </p>
-
-            <div className="questions-container">
-              <div className="questions-heading">Questions</div>
-              <div className="question-box-container">
-                <div className="question-text">
-                  According to Shodan.io, what is the 2nd country in the world
-                  in terms of the number of publicly accessible Apache servers?
-                </div>
-                <div className="anser-box-container">
-                  <input
-                    className="answer-box-input-field"
-                    placeholder="Answer"
-                    type="text"
-                  />
-                  <button className="submit-button">
-                    <div className="submit-button-text">Submit</div>
-                  </button>
-                  <button className="hint-button" onClick={() => showHint("Hint: The country is in Asia.")}>
-                    <div className="hint-button-text">Hint</div>
-                  </button>
-                </div>
-              </div>
-              <div className="question-box-container">
-                <div className="question-text">
-                  According to Shodan.io, what is the 2nd country in the world
-                  in terms of the number of publicly accessible Apache servers?
-                </div>
-                <div className="anser-box-container">
-                  <input
-                    className="answer-box-input-field"
-                    placeholder="Answer"
-                    type="text"
-                  />
-                  <button className="submit-button">
-                    <div className="submit-button-text">Submit</div>
-                  </button>
-                  <button className="hint-button" onClick={() => showHint("Hint:Hint:Hint:Hint:Hint:HintHintHintHintHintHintHintHintHint:")}>
-                    <div className="hint-button-text">Hint</div>
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-        )}
-        <div className="coursesectioncontainer" onClick={() => toggleSection(1)} >
-          <div className="coursesectionbox">
             <div className="checkboxgreencontainer">
               <img
                 className="checkboxgreen-icon"
@@ -211,13 +75,13 @@ const CoursePage: FunctionComponent = () => {
                 src="/checkboxgreen.svg"
               />
             </div>
-            <div className="dropdown-name">Passive Reconnaissance</div>
+            <div className="dropdown-name">Cryptography</div>
           </div>
           <div className="arrowdowncontainer">
             <img className="arrowdownicon" alt="" src="/arrowdownicon.svg" />
           </div>
         </div>
-        {isOpenArray[1] && (
+        {isOpenArray[0] && (
           <div className="course-section-dropdown-container">
             <p className="course-section-paragraph">
               Криптография – это наука о методах обеспечения конфиденциальности, целостности и подлинности информации. История криптографии уходит корнями в глубокую древность, начиная с простых шифров Цезаря до сложных современных алгоритмов. Основные цели криптографии включают в себя:
@@ -327,4 +191,4 @@ const CoursePage: FunctionComponent = () => {
   );
 };
 
-export default CoursePage;
+export default CoursePageCryptography;
